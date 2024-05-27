901074e040e8982730693f4552cb7b616612b597
Samuel Pineda Rodriguez <136114451+SamBarzel@users.noreply.github.com>
2024-05-27 12:15:55 -0600

    primerC

---
+++ contenido.pl
@@ -0,0 +1,31 @@
+%series
+%contenido(id,nombre,genero,año,psinopsis,capitulos,actores,idiomas,)
+%Generos_Series
+%drama
+serie(1,'La Reina de las lágrimas',drama,año,' La historia gira en torno a Baek Hyun-woo y Hong Hae-in, el matrimonio encarnado por Kim Soo-hyun y Kim Ji-won que encara una verdadera montaña rusa de emociones.',16,'Kim Ji Won, Kim Soo-hyun','Coreano y Español').
+serie(2,'La Reina de las lágrimas',drama,año,' La historia gira en torno a Baek Hyun-woo y Hong Hae-in, el matrimonio encarnado por Kim Soo-hyun y Kim Ji-won que encara una verdadera montaña rusa de emociones.',16,'Kim Ji Won, Kim Soo-hyun','Coreano y Español').
+serie(3,'La Reina de las lágrimas',drama,año,' La historia gira en torno a Baek Hyun-woo y Hong Hae-in, el matrimonio encarnado por Kim Soo-hyun y Kim Ji-won que encara una verdadera montaña rusa de emociones.',16,'Kim Ji Won, Kim Soo-hyun','Coreano y Español').
+serie(4,'La Reina de las lágrimas',drama,año,' La historia gira en torno a Baek Hyun-woo y Hong Hae-in, el matrimonio encarnado por Kim Soo-hyun y Kim Ji-won que encara una verdadera montaña rusa de emociones.',16,'Kim Ji Won, Kim Soo-hyun','Coreano y Español').
+serie(5,'La Reina de las lágrimas',drama,año,' La historia gira en torno a Baek Hyun-woo y Hong Hae-in, el matrimonio encarnado por Kim Soo-hyun y Kim Ji-won que encara una verdadera montaña rusa de emociones.',16,'Kim Ji Won, Kim Soo-hyun','Coreano y Español').
+serie(6,'La Reina de las lágrimas',drama,año,' La historia gira en torno a Baek Hyun-woo y Hong Hae-in, el matrimonio encarnado por Kim Soo-hyun y Kim Ji-won que encara una verdadera montaña rusa de emociones.',16,'Kim Ji Won, Kim Soo-hyun','Coreano y Español').
+
+%ciencia_ficcion
+
+%aventura
+
+%terror
+
+%anime
+
+%comedia
+
+%suspenso
+
+%animacion
+
+%telenovela
+
+%policial
+
+%Gebero_peliculas
+pelicula(1,'La Reina de las lágrimas',kdrama,año,' La historia gira en torno a Baek Hyun-woo y Hong Hae-in, el matrimonio encarnado por Kim Soo-hyun y Kim Ji-won que encara una verdadera montaña rusa de emociones.',16,'Kim Ji Won, Kim Soo-hyun','Coreano y Español').
